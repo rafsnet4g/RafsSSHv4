68a08edcbffa8f9f787e440dce137e16f15bc930
RAFS NET 4G <106834106+rafsnet4g@users.noreply.github.com>
2022-07-05 16:42:06 -0300

    Update websocket.sh

--- Modulos/websocket.sh
+++ Modulos/websocket.sh
@@ -32,12 +32,12 @@ fun_bar () {
 
 clear&&clear
 echo -e "\033[1;37m◆═════════════════════════════════════════════════════════════◆\033[0m"
-echo -e "\E[44;1;37m                  GERENCIAR WEBSOCKET MULTIPLEX                \E[0m"
+echo -e "\E[44;1;37m                  WEBSOCKET RAFS NET 4G SSH V4                 \E[0m"
 echo -e "\033[1;37m◆═════════════════════════════════════════════════════════════◆\033[0m"
 echo ""
 echo -e "\033[1;31m• \033[1;33mWEBSOCKET MULTIPLEX USARA A PORTA 80 e 443!\033[0m"
 echo ""
-echo -e "\033[1;33m     INSTALANDO WEBSOCKET MULTIPLEX... "
+echo -e "\033[1;33m     INSTALANDO WEBSOCKET ... "
 inst_ssl () {
 
 apt-get install stunnel4 -y
